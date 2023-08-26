d93585bb7e29949ad4601cef5791e6953b0ff6dd
Gregory Yer <93124412+Egerinik@users.noreply.github.com>
2023-08-26 12:09:28 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,27 @@
+document.querySelectorAll('.slider').forEach ((n, i) => {
+     
+window[`slider${i+1}`] = new Swiper(n, {
+    freeMode: true,
+    centeredSlides: true,
+    direction: "vertical",
+    mouseWheel: true,
+    slidesPerView: 1.75,
+    slidesOffsetBefore: -125,   
+
+  })
+});
+
+bindSwipers(slider1, slider2, slider3, slider4);
+ 
+
+// document.querySelectorAll('.slider').forEach((n, i) => {
+// 	window[`slider${i+1}`] = new Swiper(n, {
+// 		freeMode: true,
+// 		centeredSlides: true,
+// 		direction: 'vertical',
+// 		mousewheel: true,
+// 		slidesPerView: 1.75,
+// 		slidesOffsetBefore: -125
+// 	})
+// })
+// bindSwipers(slider1, slider2, slider3, slider4)
